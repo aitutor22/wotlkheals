1d56ff16cf0815605711f51a4e0873b9ee6207b2
trollhealer <dbt22@cornell.edu>
2022-08-05 23:04:14 +0800

    adding dmcg proc

--- src/common/basePlayer.js
+++ src/common/basePlayer.js
@@ -4,9 +4,16 @@ class BasePlayer {
     // procs include isCrit, isEoG, etc
     constructor(maxMana, playerClass, otherMP5, critChance, options) {
         this._options = options;
+        if (typeof this._options['logsLevel'] === 'undefined') {
+            this._options['logsLevel'] = 0;
+        }
+        if (typeof this._options['trinkets'] === 'undefined') {
+            this._options['trinkets'] = [];
+        }
+
         this._playerClass = playerClass;
         this._otherMP5 = otherMP5; // does not include raid buffs
-        this._intModifier = DATA['classes'][playerClass]['intModifier'];
+        this._intModifier = DATA['classes'][playerClass]['intModifier']; // different classes have different int modifiers
 
         // when there is dmc: greatness proc, we increase mana_pool, so need baseMaxMana as a reference
         this._baseMaxMana = maxMana;
@@ -16,8 +23,9 @@ class BasePlayer {
         for (let key of this._options['trinkets']) {
             let item = DATA['items'][key];
             if (typeof item['base']['int'] !== 'undefined') {
-                this._baseMaxMana += this.getManaIncreaseFromInt(item['base']['int']);
-                this._baseCritChance += this.getCritIncreaseFromInt(item['base']['int']);
+                this._baseMaxMana += this.manaIncreaseFromInt(item['base']['int']);
+                // console.log(this.getCritIncreaseFromInt(item['base']['int']));
+                this._baseCritChance += this.critIncreaseFromInt(item['base']['int']);
             }
         }
 
@@ -28,33 +36,63 @@ class BasePlayer {
         this.initialiseBuffs();
     }
 
+    // start getters
+    // checks for whether dmcg buff is active; if so, then uses the increased mana pool
+    get maxMana() {
+        if ((typeof this._buffs['dmcg'] !== 'undefined') && this._buffs['dmcg']['active']) {
+            return this._baseMaxMana + this.manaIncreaseFromInt(DATA['items']['dmcg']['proc']['int']);
+        } else {
+            return this._baseMaxMana;
+        }
+    }
+
+    get critChance() {
+        if ((typeof this._buffs['dmcg'] !== 'undefined') && this._buffs['dmcg']['active']) {
+            return this._baseCritChance + this.critIncreaseFromInt(DATA['items']['dmcg']['proc']['int']);
+        } else {
+            return this._baseCritChance;
+        }
+    }
+    // end getters
+
+    // start setters
+    setDmcgActive(val, timestamp) {
+        if (typeof this._buffs['dmcg'] === 'undefined') return;
+        this._buffs['dmcg']['active'] = val;
+        // if it has been toggled active, we need to track when it was last used
+        if (val) {
+            this._buffs['dmcg']['availableForUse'] = false;
+            this._buffs['dmcg']['lastUsed'] = timestamp;
+        } else {
+           this._buffs['dmcg']['availableForUse'] = false;
+        }
+        if (this._options['logsLevel'] === 2) {
+            console.log(`${timestamp}s: SETTING DMCG to ${val}`);
+            console.log(`Crit Chance: ${this.critChance}`);
+        }
+    }
+
+
+    // end setters
+
     initialiseBuffs() {
         let defaultValue = {
             active: false, // is it currently active
-            availableFor_Use: true, // can it be used; should be set to false if currently active
+            availableForUse: true, // can it be used; should be set to false if currently active
             lastUsed: -9999, // timestamp of last usage
         };
-        if (typeof this._options['trinkets']['dmcg'] !== 'undefined' && this._options['trinkets']['dmcg']) {
+        if (this._options['trinkets'].indexOf('dmcg') > -1) {
             this._buffs['dmcg'] = {};
             Object.assign(this._buffs['dmcg'], defaultValue);
         }
     }
 
-     getManaIncreaseFromInt(value) {
-         return Math.floor(value * DATA['constants']['manaFromOneInt'] * this._intModifier);
-     }
+    manaIncreaseFromInt(value) {
+        return Math.floor(value * DATA['constants']['manaFromOneInt'] * this._intModifier);
+    }
 
-     getCritIncreaseFromInt(value) {
-         return value * this._intModifier * DATA['constants']['critChanceFromOneInt'];
-     }
-
-    // checks for whether dmcg buff is active; if so, then uses the increased mana pool
-    get maxMana() {
-        if ((typeof this._buffs['dmcg'] !== 'undefined') && this._buffs['dmcg']['active']) {
-            return this._baseMaxMana + this.getManaIncreaseFromInt(DATA['items']['dmcg']['value']);
-        } else {
-            return this._baseMaxMana;
-        }
+    critIncreaseFromInt(value) {
+        return value * this._intModifier * DATA['constants']['critChanceFromOneInt'];
     }
 
     // return 'Mana: {:d} / {:d}'.format(self._current_mana, self._max_mana)
--- src/common/paladin.test.js
+++ src/common/paladin.test.js
@@ -2,17 +2,46 @@ const Paladin = require('./paladin');
 
 test('getManaIncreaseFromInt for a paladin should use 1.1 x 1.1 modifier', () => {
     let player = new Paladin(28000, 300, 0.3, {trinkets: []});
-    let manaGenerated = player.getManaIncreaseFromInt(300);
+    let manaGenerated = player.manaIncreaseFromInt(300);
     expect(manaGenerated).toBe(5445);
 });
 
 test('getCritIncreaseFromInt for a paladin should use 1.1 x 1.1 modifier', () => {
     let player = new Paladin(28000, 300, 0.3, {trinkets: []});
-    let getCritIncreaseFromInt = player.getCritIncreaseFromInt(300);
+    let getCritIncreaseFromInt = player.critIncreaseFromInt(300);
     expect((Math.abs(getCritIncreaseFromInt - 0.021779956440087123))).toBeLessThan(1e-9);
 });
 
 test('system should add mana from 90 int if dmcg is selected', () => {
     let player = new Paladin(28000, 300, 0.3, {trinkets: ['dmcg']});
     expect(player.maxMana).toBe(29633);
+});
+
+test('system should add crit from 90 int if dmcg is selected', () => {
+    let player = new Paladin(28000, 300, 0.3, {trinkets: ['dmcg']});
+    expect((Math.abs(player.critChance - 0.306533869322614))).toBeLessThan(1e-5);
+});
+
+test('setDmcgActive to true', () => {
+    let player = new Paladin(28000, 300, 0.3, {trinkets: ['dmcg']});
+    player.setDmcgActive(true, 2);
+    expect(player._buffs['dmcg']['active']).toBe(true);
+    expect(player._buffs['dmcg']['availableForUse']).toBe(false);
+    expect(player._buffs['dmcg']['lastUsed']).toBe(2);
+});
+
+test('setDmcgActive to true', () => {
+    let player = new Paladin(28000, 300, 0.3, {trinkets: ['dmcg']});
+    player.setDmcgActive(true, 2);
+    player.setDmcgActive(false, 17);
+    expect(player._buffs['dmcg']['active']).toBe(false);
+    expect(player._buffs['dmcg']['availableForUse']).toBe(false);
+    expect(player._buffs['dmcg']['lastUsed']).toBe(2);
+});
+
+test('maxMana and critChance when dmcg active', () => {
+    let player = new Paladin(28000, 300, 0.3, {trinkets: ['dmcg']});
+    player.setDmcgActive(true, 2);
+    expect(player.maxMana).toBe(35078);
+    expect((Math.abs(player.critChance - 0.328308338))).toBeLessThan(1e-5);
 });
