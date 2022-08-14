13f32bf58a9f15afee5f578d61ca3a599db88351
trollhealer <dbt22@cornell.edu>
2022-08-14 17:38:32 +0800

    works locally

--- src/app.js
+++ src/app.js
@@ -12,8 +12,6 @@ var app = express();
 
 app.use(cors());
 
-// app.use(require('connect-history-api-fallback')());
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -24,8 +22,22 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/api', apiRouter);
-app.use('/', indexRouter);
+// app.use('/', (req, res, next) => {
+
+// });
+
+/* GET home page. */
+
+app.use(express.static('public'));
+app.use('*', function(req, res, next) {
+  res.sendFile(path.resolve(__dirname, '../public/index.html'));
+});
+
+// this helps with SPA - but should come after all the server routes to avoid messing them up
+// https://forum.vuejs.org/t/how-to-handle-vue-routes-with-express-ones/23522
+app.use(require('connect-history-api-fallback')());
 
 
 // catch 404 and forward to error handler
