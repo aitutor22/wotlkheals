8d891e93fd6047d2a3aed9eb5ef3025b31c0a557
trollhealer <dbt22@cornell.edu>
2022-08-20 23:32:50 +0800

    adding

--- src/wcl/services.js
+++ src/wcl/services.js
@@ -1,16 +1,16 @@
-let axios = require('axios');
+// let axios = require('axios');
 
 const helperFunctions = {
     pullData(query) {
-        headers = {
-            'Authorization': `Bearer ${process.env.ACCESS_TOKEN}`
-        }
-        url = 'https://classic.warcraftlogs.com/api/v2/client';
-        console.log(query)
-        axios.post(url, {query: query}, {headers: headers})
-            .then((response) => {
-                console.log(response.data);
-            })
+        // headers = {
+        //     'Authorization': `Bearer ${process.env.ACCESS_TOKEN}`
+        // }
+        // url = 'https://classic.warcraftlogs.com/api/v2/client';
+        // console.log(query)
+        // axios.post(url, {query: query}, {headers: headers})
+        //     .then((response) => {
+        //         console.log(response.data);
+        //     })
     }
 }
 
