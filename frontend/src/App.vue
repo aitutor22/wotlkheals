bfa42603651f98711cc1a954ad8d3b160d924c75
trollhealer <dbt22@cornell.edu>
2022-08-20 16:29:21 +0800

    hiding link

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,8 +2,8 @@
   <div id="app">
     <nav>
       <router-link to="/">Home</router-link> |
-      <router-link to="/paladin/ttoom/">Paladin TTOOM</router-link> | 
-      <router-link to="/paladin/statweights/">Paladin Stat Weights</router-link>
+      <router-link to="/paladin/ttoom/">Paladin TTOOM</router-link>
+      <!-- <router-link to="/paladin/statweights/">Paladin Stat Weights</router-link> -->
     </nav>
     <router-view/>
   </div>
