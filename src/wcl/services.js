13719595e8000b664ef227e9734360cb2b1bd305
trollhealer <dbt22@cornell.edu>
2022-08-20 20:17:52 +0800

    base

---
+++ src/wcl/services.js
@@ -0,0 +1,22 @@
+console.log(process.env)
+
+const helperFunctions = {
+    pullData(query) {
+        headers = {
+            // 'Authorization': `Bearer ${}`
+        }
+    }
+}
+
+// def pull_data(query):
+//     headers = {
+//         'Authorization': 'Bearer {}'.format(access_token)
+//     }
+//     url = 'https://classic.warcraftlogs.com/api/v2/client'
+//     try:
+//         r = requests.post(url, json={'query': query}, headers=headers)
+//         return r.json()
+//     except Exception as e:
+//         print(e)
+
+module.exports = helperFunctions;
